f99aa9a596863f7d3aadbbaa5ab28b558901c8d5
Jared Guerrero <43221406+jaredgs93@users.noreply.github.com>
2022-09-13 11:55:53 +0200

    Update video_details.py

--- video_details.py
+++ video_details.py
@@ -1,12 +1,8 @@
 #import ffmpeg
 import streamlit as st
 import os
-root_folder = '/videos/'
+root_folder = 'videos/'
 video = 'Sujeto35.mp4'
 ruta_video = root_folder+video
-"""j = ffmpeg.probe(ruta_video)
-st.json(j)"""
+st.json({'ruta_existente':os.path.exists(ruta_video)})
 
-
-files = [f for f in os.listdir('.')]
-files
