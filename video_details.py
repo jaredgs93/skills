14a37b6798dd3307f48510e4dde896916a7b97d1
Jared Guerrero <43221406+jaredgs93@users.noreply.github.com>
2022-09-13 11:57:12 +0200

    Update video_details.py

--- video_details.py
+++ video_details.py
@@ -6,3 +6,7 @@ video = 'Sujeto35.mp4'
 ruta_video = root_folder+video
 st.json({'ruta_existente':os.path.exists(ruta_video)})
 
+video_file = open('ruta_video', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
