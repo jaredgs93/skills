ed20231acb8c421bd79179f535300c88a6f91fdd
Jared Guerrero <43221406+jaredgs93@users.noreply.github.com>
2022-09-13 13:01:16 +0200

    Update video_details.py

--- video_details.py
+++ video_details.py
@@ -9,7 +9,7 @@ root_folder = 'videos/'
 video = 'Sujeto35.mp4'
 ruta_video = root_folder+video
 
-#j = ffmpeg.probe(ruta_video)
+j = ffmpeg.probe(ruta_video)
 #st.json(j)
 
 video_file = open(ruta_video, 'rb')
