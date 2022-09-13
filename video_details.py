b629e82e7c95f811165df936784d1920bc6f5c1a
Jared Guerrero <43221406+jaredgs93@users.noreply.github.com>
2022-09-13 11:45:52 +0200

    Update video_details.py

--- video_details.py
+++ video_details.py
@@ -1,7 +1,7 @@
 import ffmpeg
 import streamlit as st
 
-root_folder = '/Users/jaredguerrero/Documents/Doctorado UCLM/Proyectos SMILE/video/'
+root_folder = '/videos/'
 video = 'Sujeto10.mp4'
 ruta_video = root_folder+video
 j = ffmpeg.probe(ruta_video)
