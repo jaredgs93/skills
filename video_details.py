033e3887ee892ba448f9c89c3a4844aa3145b3f2
Jared Guerrero <43221406+jaredgs93@users.noreply.github.com>
2022-09-13 12:00:43 +0200

    Update video_details.py

--- video_details.py
+++ video_details.py
@@ -4,7 +4,9 @@ import os
 root_folder = 'videos/'
 video = 'Sujeto35.mp4'
 ruta_video = root_folder+video
-st.json({'ruta_existente':os.path.exists(ruta_video)})
+
+j = ffmpeg.probe(ruta_video)
+st.json(j)
 
 video_file = open(ruta_video, 'rb')
 video_bytes = video_file.read()
