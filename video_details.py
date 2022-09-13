b5b7b47d701e982c633be742d2403f0f4a5e8320
Jared Guerrero <43221406+jaredgs93@users.noreply.github.com>
2022-09-13 16:40:20 +0200

    Update video_details.py

--- video_details.py
+++ video_details.py
@@ -1,18 +1,19 @@
-import ffmpeg
+#import ffmpeg
 import streamlit as st
 import os
 import sys
 #sys.path.append('ffmpeg/')
 
-st.json({'Exists':os.path.exists('ffmpeg/')})
+#st.json({'Exists':os.path.exists('ffmpeg/')})
 root_folder = 'videos/'
 video = 'Sujeto35.mp4'
 ruta_video = root_folder+video
 
-j = ffmpeg.probe(ruta_video)
+#j = ffmpeg.probe(ruta_video)
 #st.json(j)
 
 video_file = open(ruta_video, 'rb')
 video_bytes = video_file.read()
 
 st.video(video_bytes)
+print(ruta_video)
