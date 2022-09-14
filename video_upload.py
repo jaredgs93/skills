9aba15d3e0cb455114d765050389f9f09c5fb139
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2022-09-14 19:49:10 +0200

    14 sep cambios 8

--- video_upload.py
+++ video_upload.py
@@ -2,6 +2,14 @@ import os
 os.system("static_ffmpeg -version")
 os.system("static_ffprobe -version")
 
+import subprocess
+from static_ffmpeg import run
+# Platform binaries are installed on the first run of below.
+ffmpeg, ffprobe = run.get_or_fetch_platform_executables_else_raise()
+# ffmpeg, ffprobe will be paths to ffmpeg and ffprobe.
+subprocess.check_output([ffmpeg, "-version"])
+subprocess.check_output([ffprobe, "-version"])
+
 import streamlit as st
 import requests
 import video_details
