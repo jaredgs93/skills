ed652bbf95737622eec7d1ab1a458db3d121fce3
Jared Guerrero <jaredguerrero@Air-de-Jared-2.uclm.es>
2022-09-14 13:33:11 +0200

    14 sep 1

--- video_details.py
+++ video_details.py
@@ -1,28 +1,7 @@
-#import ffmpeg
+import ffmpeg
 import streamlit as st
-import os
-import sys
-#sys.path.append('ffmpeg/')
+import exiftool
 
-#st.json({'Exists':os.path.exists('ffmpeg/')})
-root_folder = 'videos/'
-video = 'VID_20190610_205528543.mp4'
-ruta_video = root_folder+video
-
-#j = ffmpeg.probe(ruta_video)
-#st.json(j)
-
-video_file = open(ruta_video, 'rb')
-video_bytes = video_file.read()
-
-st.video(video_bytes)
-
-#import subprocess
-
-#list_files = subprocess.run(["/home/appuser/venv/bin/bash/ -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""])
-#list_files = subprocess.run(["git clone https://github.com/Homebrew/brew homebrew"])
-#"The exit code was: %d" % list_files.returncode
-
-#st.markdown([x[0] for x in os.walk('/home/appuser/')])
-
-#st.markdown([f.path for f in os.scandir('/home/appuser/venv/') if f.is_dir()])
+def show_metadata(video_url):
+    vid = ffmpeg.probe(video_url)
+    st.json(vid)
--- video_upload.py
+++ video_upload.py
@@ -1,5 +1,13 @@
 import streamlit as st
+import video_details
+#URL prueba: https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
 
-title = st.text_input('URL del video', '')
+st.set_page_config(page_title="Carga de video")
 
-st.video('https://youtu.be/rYZYCA2EoxE')
+url_video = st.text_input('URL del video', '')
+send_video = st.button('Cargar video')
+if send_video:
+    try:
+        video_details.show_metadata(url_video)
+    except:
+        st.warning('URL no válida. Intentar de nuevo.', icon="⚠️")
