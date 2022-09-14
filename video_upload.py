c78a41069ed16bed478c2cd30c67666b337f5e21
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2022-09-14 16:23:56 +0200

    14 sep 14

--- video_upload.py
+++ video_upload.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import video_details
 import os
-os.system("apt install sudo")
+os.system("ps aux | grep -i apt")
 #URL prueba: https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
 
 st.set_page_config(page_title="Carga de video")
