b34838e29433c30a2e184b7ff5fee3e67d44fb38
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2022-09-14 17:07:15 +0200

    14 sep 16

--- video_details.py
+++ video_details.py
@@ -1,7 +1,11 @@
-import ffmpeg
+#import ffmpeg
+from videoprops import get_video_properties
 import streamlit as st
 
 def show_metadata(video_url):
-    vid = ffmpeg.probe(video_url)
-    st.json(vid)
+    #vid = ffmpeg.probe(video_url)
+    #st.json(vid)
+    props = get_video_properties(video_url)
+    print(props)
+    st.json(props)
     
--- video_upload.py
+++ video_upload.py
@@ -1,6 +1,6 @@
 import streamlit as st
+import requests
 import video_details
-
 #URL prueba: https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
 
 st.set_page_config(page_title="Carga de video")
@@ -9,6 +9,13 @@ url_video = st.text_input('URL del video', '')
 send_video = st.button('Cargar video')
 if send_video:
     #try:
-        video_details.show_metadata(url_video)
+        file_name = 'videos/trial_video.mp4' 
+        resp = requests.get(url_video) # making requests to server
+
+        with open(file_name, "wb") as f: # opening a file handler to create new file 
+            f.write(resp.content)
+        video_details.show_metadata(file_name)
+        
+
     #except:
         #st.warning('URL no válida. Intentar de nuevo.', icon="⚠️")
