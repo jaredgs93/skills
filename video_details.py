d4790e5ec7a36e3ec345d71b882a7cd09fe565c2
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2022-09-14 19:42:51 +0200

    14 sep cambios 6

--- video_details.py
+++ video_details.py
@@ -1,8 +1,8 @@
-#import ffmpeg
+import ffmpeg
 
 #Remover comentarios
 #from videoprops import get_video_properties
-#import streamlit as st
+import streamlit as st
 
 def show_metadata(video_url):
     print(video_url)
@@ -11,4 +11,6 @@ def show_metadata(video_url):
     #props = get_video_properties(video_url)
     #print(props)
     #st.json(props)
-    
+    metadata = ffmpeg.probe("videos/VID_20190610_205528543.mp4")
+    st.json(metadata)
+    #print(metadata)
--- video_upload.py
+++ video_upload.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import requests
 import video_details
-#URL prueba: https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
-import os
 
-os.system("static_ffmpeg -version")
+
+#URL prueba: https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
+#import os
+
+#os.system("static_ffmpeg -version")
+#os.system("static_ffprobe -version")
 
 st.set_page_config(page_title="Carga de video")
 
@@ -12,12 +15,12 @@ url_video = st.text_input('URL del video', '')
 send_video = st.button('Cargar video')
 if send_video:
     #try:
-        file_name = 'trial_video.mp4' 
-        resp = requests.get(url_video) # making requests to server
+        #file_name = 'trial_video.mp4' 
+        #resp = requests.get(url_video) # making requests to server
 
-        with open(file_name, "wb") as f: # opening a file handler to create new file 
-            f.write(resp.content)
-        #video_details.show_metadata(file_name)
+        #with open(file_name, "wb") as f: # opening a file handler to create new file 
+        #    f.write(resp.content)
+        video_details.show_metadata(url_video)
         
 
     #except Exception as e:
