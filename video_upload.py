6022edd6932ce89db9ab5c31511ec00170188006
Jared Guerrero <jaredguerrero@MacBook-Air-de-Jared-2.local>
2022-09-14 16:05:16 +0200

    14 sep 7

--- video_upload.py
+++ video_upload.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import video_details
 import os
-os.system("sudo apt-get install ffmpeg")
+os.system("sudo")
 #URL prueba: https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
 
 st.set_page_config(page_title="Carga de video")
